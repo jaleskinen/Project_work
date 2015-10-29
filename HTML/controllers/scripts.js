6ae04d336381751f8e5e1183b2d00fd0bbc00dea
jaleskinen <jarmo.leskinen@hepokumpu.com>
2015-10-29 18:13:03 +0200

    Version 0.12. Jarmo Leskinen. Bootstrap updates.

--- HTML/controllers/scripts.js
+++ HTML/controllers/scripts.js
@@ -1,1 +1,31 @@
-console.log("Project work console");
+console.log("!! Project work console works OK !!");
+
+/*$(document).ready(function () { 
+    
+    console.log("!! jquery onload triggered !!");
+
+    var setting =  {
+        
+        method: "GET",
+        url: "http://localhost:28017/testi/person/",
+        dataType: "jsonp",
+        jsonp: "jsonp"
+        
+    };
+    
+    
+    $.ajax(setting).done(function(data) {
+        console.log(data);
+        for (i = 0; i < data.rows.length; i++) {
+            
+            var html = "<tr>" +
+                        "<td>" + data.rows[i].name + "</td>" +
+                        "<td>" + data.rows[i].address + "</td>" +
+                        "<td>" + data.rows[i].age + "</td>" +
+                        "</tr>";
+            
+            $(html).appendTo("tbody"); 
+            
+        }
+    });
+});*/
